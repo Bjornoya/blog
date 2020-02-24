b6f6bac614c8b8348d5b8c44e541122847efdf60
Gleb Ambrazhevich <damndollar@gmail.com>
2020-02-25 01:37:55 +0500

    Created helper function to test propTypes

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,12 @@
+import checkPropTypes from 'check-prop-types';
+
+
 export const findByTestAttr = (component, attr) => {
   const wrapper = component.find(`[data-test='${attr}']`);
   return wrapper;
+};
+
+export const propTypesCheck = (component, props) => {
+  const result = checkPropTypes(component.propTypes, props, 'props', component.name);
+  return result;
 };
