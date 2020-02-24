38196e7549d388fcef5ad6902674930d46411bc0
Gleb Ambrazhevich <damndollar@gmail.com>
2020-02-25 01:53:58 +0500

    Added styling to Button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import style from './Button.module.scss';
 
 const Button = ({ children }) => {
     if(children) {
         return (
-            <div data-test="button">
+            <div className={style.button} data-test="button">
                 {children}
             </div>
         );
