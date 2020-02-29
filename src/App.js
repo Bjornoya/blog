b5f60aeac13934ec65bbdca25001b4e5d479e1bd
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-01 01:28:13 +0500

    Added connect to redux in App.js

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import style from './App.module.scss';
 import Header from "./components/Header/Header";
 import Post from "./components/Post/Post";
 import News from "./components/News/News";
+import { useSelector } from "react-redux";
 
-const posts = [{
-    "userId": 1,
-    "id": 1,
-    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-},{
-    "userId": 1,
-    "id": 2,
-    "title": "qui est esse",
-    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-},];
 
 function App() {
+
+    const newsData = useSelector(state => state.news.data);
+
   return (
     <div>
       <Header />
-      <News data={posts} />
+      <News data={newsData} />
       <Post />
     </div>
   );
