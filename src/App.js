1b53794697f6c8ba9a83d5b32483b57a4af0e309
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-01 18:15:26 +0500

    Added redux-hooks logic to App component. Created test for useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from './App.module.scss';
 import Header from "./components/Header/Header";
 import Post from "./components/Post/Post";
 import News from "./components/News/News";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { getNews } from "./actions/actionCreators";
 
 
 function App() {
+
+    useEffect(() => {
+        dispatch(getNews())
+    }, []);
+
+    const dispatch = useDispatch();
 
     const newsData = useSelector(state => state.news.data);
 
