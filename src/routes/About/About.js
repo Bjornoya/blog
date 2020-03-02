bc58328306c6cfddeed1f3f42118b6f582a0b5d6
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-02 14:13:20 +0500

    Created folder with routes

---
+++ src/routes/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../../components/Header/Header";
+
+const About = () => {
+    return (
+        <>
+            <Header />
+            <div>
+                About page
+            </div>
+        </>
+    );
+};
+
+export default About;
---
+++ src/routes/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../../components/Header/Header";
+
+const Contact = () => {
+    return (
+        <>
+            <Header />
+            <div>
+                Contact Us
+            </div>
+        </>
+    );
+};
+
+export default Contact;
