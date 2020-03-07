ca26f0ba5a7f479095e41db91d8a80911bdeffcf
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-07 13:48:13 +0500

    Created styling for Post component. Added image prop

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -2,14 +2,17 @@ import React from 'react';
 import style from './Post.module.scss';
 import PropTypes from 'prop-types';
 
-const Post = ({ userId, title, body, id }) => {
-    if (userId && id && title && body) {
+const Post = ({ userId, title, body, image, id }) => {
+    if (userId && id && title && body && image) {
         return (
             <div className={style.post} data-test="post">
-                <h2 data-test="userId">Id пользователя: {userId}</h2>
-                <h4>Заголовок: {title}</h4>
-                <p>Номер поста: {id}</p>
-                <p>Текст новости: {body}</p>
+                <div className={style.image} style={{backgroundImage: `url(${image})`}}/>
+                <div className={style.body}>
+                    <p className={style.num}>№{id}</p>
+                    <p className={style.userId} data-test="userId">User ID {userId}</p>
+                    <h2 className={style.title}>{title}</h2>
+                    <p className={style.text}>{body}</p>
+                </div>
             </div>
         );
     } else {
@@ -21,7 +24,8 @@ Post.propTypes = {
     userId: PropTypes.number,
     title: PropTypes.string,
     body: PropTypes.string,
-    id: PropTypes.number
+    id: PropTypes.number,
+    image: PropTypes.string
 };
 
 export default Post;
--- src/components/Post/Post.test.js
+++ src/components/Post/Post.test.js
@@ -12,7 +12,8 @@ describe('Post component', () => {
             "userId": 1,
             "id": 1,
             "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+            "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto",
+            "image": "http://test.image.com"
         };
 
         const setUp = () => {
@@ -32,7 +33,7 @@ describe('Post component', () => {
 
         it('Should render correct userId', () => {
             const wrapper = findByTestAttr(component, 'userId');
-            expect(wrapper.text().includes('Id пользователя: 1')).toBe(true);
+            expect(wrapper.text().includes('User ID 1')).toBe(true);
         });
 
         it('Should receive valid propTypes', () => {
