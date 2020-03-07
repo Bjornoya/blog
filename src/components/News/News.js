6f7b615b1008375e8fb97eac56605abac74d3cb7
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-07 13:46:10 +0500

    Fixed margins in News component. Added prop 'image'. Downloaded default image to assets

--- src/components/News/News.js
+++ src/components/News/News.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import style from './News.module.scss';
 import Post from "../Post/Post";
 
+import image from '../../assets/img/iceland.jpg'
+
 // TODO: `Обратить внимание на то, что константа posts лежит внутри блока if.
 //  Ночью сломал себе мозг пытаясь понять почему получал undefined из data.
 //  Этим if-else я даю понять, что переменная должна создавать только когда пришли пропсы во избежание ошибок.
@@ -12,7 +14,7 @@ const News = ({ data }) => {
 
     if (data) {
         const posts = data.map((post, index) => {
-            return <Post userId={post.userId} id={index} title={post.title} body={post.body} key={index} />
+            return <Post userId={post.userId} id={index} title={post.title} body={post.body} image={image} key={index} />
         });
 
         return (
