74e70161963aea591694b05a5298e950f62a5d56
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-08 02:02:46 +0500

    Created Input component

---
+++ src/components/Input/Input.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Input.module.scss';
+
+
+const Input = () => {
+    return (
+        <input className={style.input} type="text" />
+    );
+};
+
+export default Input;
