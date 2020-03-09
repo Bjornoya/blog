f47edb7204408a131d1e6c600d5a8a2c549dae30
Gleb Ambrazhevich <damndollar@gmail.com>
2020-03-09 20:20:21 +0500

    Created Tooltip component

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './Tooltip.module.scss';
+import Icon from "../Icon/Icon";
+
+const Tooltip = ({ className, children, toolTipIsOpen }) => {
+    if (toolTipIsOpen) {
+        return (
+            <div className={style.wrapper}>
+                <div className={style.item}>
+                    <Icon icon="edit" />
+                    Edit post
+                </div>
+                <div className={style.item}>
+                    <Icon icon="delete" />
+                    Delete post
+                </div>
+            </div>
+        );
+    } else {
+        return null;
+    }
+};
+
+export default Tooltip;
